751effa5a65cc697dcb15ea053cd49e654bd2075
Freddy <freddypalencia125@gmail.com>
2025-11-18 18:01:23 -0300

    Update redirect in precio page to use permanent status code (301) for SEO optimization

--- pages/precio/index.vue
+++ pages/precio/index.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
   asyncData({ redirect }) {
-    // Redirigir a peso-chileno por defecto
-    return redirect('/precio/peso-chileno')
+    // Redirigir a peso-chileno por defecto (301 = permanente para SEO)
+    return redirect(301, '/precio/peso-chileno')
   }
 }
 </script>
